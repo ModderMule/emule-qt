cf5dd9b76b3a90c9de7c80393ed9760d287181d4
Ekliptor <Ekliptor1@yandex.com>
2026-03-06 20:07:21 +0700

    added VS PCH and relative dependency paths

---
+++ src/core/pch.h
@@ -0,0 +1,37 @@
+#pragma once
+// Precompiled header for emulecore (mirrors CMakeLists.txt target_precompile_headers)
+
+// C++ standard library
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+// Qt Core
+#include <QByteArray>
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QHostAddress>
+#include <QIODevice>
+#include <QList>
+#include <QMutex>
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QThread>
+#include <QTimer>
