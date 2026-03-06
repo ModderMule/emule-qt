67d971d2755a765e8aa6f1edf2288e2d8940290c
ModderMule <dev@emule-qt.org>
2026-03-06 20:07:21 +0700

    added VS PCH and relative dependency paths

---
+++ src/gui/pch.h
@@ -0,0 +1,36 @@
+#pragma once
+// Precompiled header for emuleqt (mirrors CMakeLists.txt target_precompile_headers)
+
+// C++ standard library
+#include <algorithm>
+#include <vector>
+
+// Qt Core
+#include <QByteArray>
+#include <QCborArray>
+#include <QCborMap>
+#include <QDateTime>
+#include <QList>
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QTimer>
+
+// Qt Widgets
+#include <QAbstractTableModel>
+#include <QApplication>
+#include <QDialog>
+#include <QFont>
+#include <QHBoxLayout>
+#include <QHeaderView>
+#include <QIcon>
+#include <QLabel>
+#include <QLineEdit>
+#include <QMenu>
+#include <QPainter>
+#include <QPushButton>
+#include <QSortFilterProxyModel>
+#include <QSplitter>
+#include <QTreeView>
+#include <QVBoxLayout>
+#include <QWidget>
