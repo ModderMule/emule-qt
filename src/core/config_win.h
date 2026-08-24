181c0e40b5ce5b9d3c06bef93aa31b057d62337a
ModderMule <dev@emule-qt.org>
2026-08-24 18:02:43 +0700

    release: v0.4.1

--- src/core/config_win.h
+++ src/core/config_win.h
@@ -10,8 +10,8 @@
 // ---------------------------------------------------------------------------
 #define EMULE_VERSION_MAJOR  0
 #define EMULE_VERSION_MINOR  4
-#define EMULE_VERSION_PATCH  0
-#define EMULE_VERSION_STRING "0.4.0"
+#define EMULE_VERSION_PATCH  1
+#define EMULE_VERSION_STRING "0.4.1"
 
 // ---------------------------------------------------------------------------
 // Platform detection
